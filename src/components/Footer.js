c6969cc76da42a29d6d2a901723d7b982657c428
hebrohim <raheemibrohim@mail.com>
2023-07-29 17:37:36 +0100

    add footer responsiveness

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,16 @@ import React from 'react'
 import instagram from "../images/instagram.png"
 import facebook from "../images/facebook.png"
 import twitter from "../images/twitter.png"
+import Logo from"../images/Logo.png"
 const Footer = () => {
   return (
-    <div className=' px-4 py-10 '>
+    <footer>
+    <div className=' px-4 py-10 lg:flex lg:justify-between'>
+        <div className='hidden lg:block'>
+<img src={Logo}/>
+        </div>
+
+        <div className='lg:flex lg:justify-between lg:w-1/2'>
         <section className='font-bold mb-8'>
             <h3 className='mb-2'>Explore</h3>
             <p className='font-medium text-[#374558] mb-1'>Destinations</p>
@@ -28,21 +35,25 @@ const Footer = () => {
             
         </section>
 
-        <div className='bg-[#374558] h-[1px]'>
-
+        
         </div>
-        
-
-        <section className='mt-7 flex justify-center items-center'>
-             
-                <img src= {instagram} className='mr-6'/>
-                <img src= {facebook} className='mr-6'/>
-                <img src= {twitter} className=''/>
-
-            
-        </section>
-        
     </div>
+
+
+    <div className='bg-[#374558] h-[1px]'>
+
+</div>
+
+
+<section className='mt-7 flex justify-center items-center'>
+     
+        <img src= {instagram} className='mr-6'/>
+        <img src= {facebook} className='mr-6'/>
+        <img src= {twitter} className=''/>
+
+    
+</section>  
+    </footer>
   )
 }
 
